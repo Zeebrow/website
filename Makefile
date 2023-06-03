69dcb26e57c239d1973902abf68b5142047d2933
Zeebrow <mzborowski@yahoo.com>
2023-06-03 15:41:09 -0400

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 ARTIFACT := website-src.tar.gz
-package:
+CONTAINER_NAME := website
+
+dev-up:
+	docker run -d --name $(CONTAINER_NAME) -p80:80 -v ./src:/usr/share/nginx/html nginx
+
+dev-down:
+	docker rm -f $(CONTAINER_NAME)
+
+package: clean
 	mkdir -p build
 	tar -C src/ -czf build/$(ARTIFACT) ./
 	sha256sum build/$(ARTIFACT) > build/SHA256SUMS
--- src/site.js
+++ src/site.js
@@ -1,11 +1,39 @@
 console.log(window.screen.width)
-if (window.screen.width <= 1080) {
-    console.log("made it")
-    document.getElementsByClassName("sidebar-left")[0].style.display = "none";
-    document.getElementsByClassName("sidebar-right")[0].style.display = "none";
-    const dividers = document.getElementsByClassName("content-divider");
-    for (let d of dividers) {
-        d.style.display = "none";
+
+function setSmolScreenContent() {
+
+    if (window.screen.width <= 601) {
+        console.log("smol device")
+        document.getElementsByClassName("sidebar-left")[0].style.display = "none";
+        document.getElementsByClassName("sidebar-right")[0].style.display = "none";
+        const dividers = document.getElementsByClassName("content-divider");
+        for (let d of dividers) {
+            d.style.display = "none";
+        }
+        const menubarCenteredItems = document.querySelectorAll("div.site-menubar");
+        for (let i of menubarCenteredItems) {
+            i.style.display = "none";
+        }
+
+        const siteLogoLink = document.createElement("a");
+        siteLogoLink.href = "/";
+
+        const siteLogo = document.createElement("img");
+        siteLogo.src = "img/site-logo.jpeg";
+        siteLogo.style.display = "block";
+        siteLogo.style.margin = "0 auto";
+        siteLogo.setAttribute("width", "50px");
+        siteLogo.setAttribute("height", "50px");
+
+        siteLogoLink.appendChild(siteLogo);
+
+        const seeDesktopPageBanner = document.createElement("p");
+        seeDesktopPageBanner.innerHTML = "Rotate your screen and refresh the page to see more content!";
+        seeDesktopPageBanner.style.cssText = "text-align: center"
+
+        document.body.prepend(seeDesktopPageBanner);
+        document.body.prepend(siteLogoLink);
     }
 }
 
+setSmolScreenContent()
